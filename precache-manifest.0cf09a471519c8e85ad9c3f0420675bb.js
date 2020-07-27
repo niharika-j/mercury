ae5ac63cf9eced6abd54f584ef51d0afab9fb761
Niharika Jayanthi <niharika.jayanthi06@gmail.com>
2020-07-27 07:21:31 -0400

    Updates

---
+++ precache-manifest.0cf09a471519c8e85ad9c3f0420675bb.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5617748c92333e8b2623943e557d5ac8",
+    "url": "/mercury/index.html"
+  },
+  {
+    "revision": "0fa924fc47a3da49fb84",
+    "url": "/mercury/static/css/main.1a797926.chunk.css"
+  },
+  {
+    "revision": "27be2f799dbb4b721816",
+    "url": "/mercury/static/js/2.b2202dab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mercury/static/js/2.b2202dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa924fc47a3da49fb84",
+    "url": "/mercury/static/js/main.14a87250.chunk.js"
+  },
+  {
+    "revision": "286119e22ce58935518b",
+    "url": "/mercury/static/js/runtime-main.983c2cbc.js"
+  },
+  {
+    "revision": "d0c05571730111dc35be1afee52e61be",
+    "url": "/mercury/static/media/booth-interview.d0c05571.png"
+  },
+  {
+    "revision": "325b7ec3d858e9d78a6cb0c18ec3c50c",
+    "url": "/mercury/static/media/clo-bank-app.325b7ec3.png"
+  },
+  {
+    "revision": "7cade857d50f0f573ae03235eea845e0",
+    "url": "/mercury/static/media/clo-publisher-app.7cade857.png"
+  },
+  {
+    "revision": "9b1fbdf5281ecefc9abe0f2985628245",
+    "url": "/mercury/static/media/clo-section-header.9b1fbdf5.png"
+  },
+  {
+    "revision": "4d5312d9dad39eaf1ae7f55ba422e8bd",
+    "url": "/mercury/static/media/consumer-app-subsection.4d5312d9.png"
+  },
+  {
+    "revision": "4e38c58e5b2c4bef874d9bbbd7e28f73",
+    "url": "/mercury/static/media/consumer-app.4e38c58e.png"
+  },
+  {
+    "revision": "eb7e0bb34635110f08b9452c40c7cce0",
+    "url": "/mercury/static/media/consumer-deal.eb7e0bb3.png"
+  },
+  {
+    "revision": "a01e54cd5c858fe3e97ca4ea40b8c60d",
+    "url": "/mercury/static/media/consumer-feed.a01e54cd.png"
+  },
+  {
+    "revision": "c17e8e1bfd589dfcd9717e8158f287e0",
+    "url": "/mercury/static/media/consumer-map.c17e8e1b.png"
+  },
+  {
+    "revision": "a28537eb06aba1fe5756e0c60b270136",
+    "url": "/mercury/static/media/consumer-persona-1.a28537eb.jpg"
+  },
+  {
+    "revision": "d9beebe568e6bd7213264b2bbc67b1b8",
+    "url": "/mercury/static/media/consumer-persona-2.d9beebe5.jpg"
+  },
+  {
+    "revision": "2cac49b52f94fbfea8d0bfd7bd1da5ab",
+    "url": "/mercury/static/media/consumer-persona-3.2cac49b5.jpg"
+  },
+  {
+    "revision": "8c7efeaceb8434e9b7e82444c8dba773",
+    "url": "/mercury/static/media/consumer-persona-4.8c7efeac.jpg"
+  },
+  {
+    "revision": "232bd13a56bea6b65843cb37e70e6015",
+    "url": "/mercury/static/media/consumer-prototypes.232bd13a.png"
+  },
+  {
+    "revision": "047f91c9d37931ccf3510e8127ab6f50",
+    "url": "/mercury/static/media/consumer-review.047f91c9.png"
+  },
+  {
+    "revision": "9f0c4f435861901ef6d27ec921aa665f",
+    "url": "/mercury/static/media/consumer-toplist.9f0c4f43.png"
+  },
+  {
+    "revision": "fadf17018cabdf0e6e03956bb5a3e0ce",
+    "url": "/mercury/static/media/crazy8.fadf1701.png"
+  },
+  {
+    "revision": "c5a75060c7aeb45e01c3e298db300f2d",
+    "url": "/mercury/static/media/dataFlow.c5a75060.png"
+  },
+  {
+    "revision": "d24cecea5c4f05cf5bdc5466e305a3c8",
+    "url": "/mercury/static/media/guerilla-research.d24cecea.png"
+  },
+  {
+    "revision": "469211f45861a4cec9f72a86029aa9e9",
+    "url": "/mercury/static/media/ideation-header.469211f4.png"
+  },
+  {
+    "revision": "863ea121369c76143b476a52466d8386",
+    "url": "/mercury/static/media/marketsize.863ea121.png"
+  },
+  {
+    "revision": "eae16479523ae4d1b396b1dd6969998a",
+    "url": "/mercury/static/media/mashup.eae16479.png"
+  },
+  {
+    "revision": "639b6bfe069a3b537ba154e689bfd9cd",
+    "url": "/mercury/static/media/merchant-app-subsection.639b6bfe.png"
+  },
+  {
+    "revision": "f1b9f1e90587960c21fed57415b55ce1",
+    "url": "/mercury/static/media/merchant-app.f1b9f1e9.png"
+  },
+  {
+    "revision": "06481e1a287e96a3d4ae5f6e939526db",
+    "url": "/mercury/static/media/merchant-interview.06481e1a.png"
+  },
+  {
+    "revision": "d04cc142a0a3ed1f4a47bb22f7b6651f",
+    "url": "/mercury/static/media/merchant-message.d04cc142.png"
+  },
+  {
+    "revision": "10736966882a0ab384c4a494a508fd67",
+    "url": "/mercury/static/media/merchant-metric.10736966.png"
+  },
+  {
+    "revision": "e3871d52b8f20de756dfc01eb1c1c788",
+    "url": "/mercury/static/media/merchant-new-deal.e3871d52.png"
+  },
+  {
+    "revision": "2f1ec6e04685d56665912ebc6482f340",
+    "url": "/mercury/static/media/merchant-profile.2f1ec6e0.png"
+  },
+  {
+    "revision": "4af3fe72a8bca4d245ca4a38ddaa24ec",
+    "url": "/mercury/static/media/merchant-prototype.4af3fe72.png"
+  },
+  {
+    "revision": "0f5cc9de8d64dde026471dd07942c8df",
+    "url": "/mercury/static/media/metric.0f5cc9de.png"
+  },
+  {
+    "revision": "1c1d1db4baa4dd99c2cc63c6e7d155d8",
+    "url": "/mercury/static/media/numo-logo.1c1d1db4.png"
+  },
+  {
+    "revision": "a8e798eada36af0851fdb33b7e8c28ae",
+    "url": "/mercury/static/media/pos-metric.a8e798ea.png"
+  },
+  {
+    "revision": "e2d0274472b3abf80f5d61c8ad7f7503",
+    "url": "/mercury/static/media/pos-onboarding.e2d02744.png"
+  },
+  {
+    "revision": "a06fc7be6b7d85929f2786b1f16d94e7",
+    "url": "/mercury/static/media/pos-saving.a06fc7be.png"
+  },
+  {
+    "revision": "708a3871e21656c2359245d80c99f40a",
+    "url": "/mercury/static/media/primary-consumer.708a3871.png"
+  },
+  {
+    "revision": "9448d0c1a3f5330a7f7f92644e3189bf",
+    "url": "/mercury/static/media/primary-merchant.9448d0c1.png"
+  },
+  {
+    "revision": "aae85e0f89cda4e6f0d01a104cb6feb7",
+    "url": "/mercury/static/media/prob-statement-bg.aae85e0f.png"
+  },
+  {
+    "revision": "4373b3ae98ec4c554154feecc5f351b8",
+    "url": "/mercury/static/media/project-final-product.4373b3ae.png"
+  },
+  {
+    "revision": "10d5fd07130af1855ca76efb432bb258",
+    "url": "/mercury/static/media/project-ideating-prototyping.10d5fd07.png"
+  },
+  {
+    "revision": "b296102b4bd1d3158a7761529d6607ad",
+    "url": "/mercury/static/media/project-research.b296102b.png"
+  },
+  {
+    "revision": "86d2535a44761158f2512f6ef952823b",
+    "url": "/mercury/static/media/prototypes-header.86d2535a.png"
+  },
+  {
+    "revision": "cc4d30d307978ceb1644bce61afd05f7",
+    "url": "/mercury/static/media/research-header.cc4d30d3.png"
+  },
+  {
+    "revision": "8f37576c5c7b9ff79ea0b6351eb909d4",
+    "url": "/mercury/static/media/section-header-solution.8f37576c.png"
+  },
+  {
+    "revision": "9aa1fef69336a200d031955b93cab78e",
+    "url": "/mercury/static/media/semi-structure-interview.9aa1fef6.png"
+  },
+  {
+    "revision": "5ba1870771479c892110e03dd33ee76c",
+    "url": "/mercury/static/media/solution-header.5ba18707.png"
+  },
+  {
+    "revision": "75a726f63e8b8ddccf5f5b8522d7eb30",
+    "url": "/mercury/static/media/solutions-header.75a726f6.png"
+  },
+  {
+    "revision": "2d2c70dbbe224331665ccf5aa7c3f5b1",
+    "url": "/mercury/static/media/stakeholder-map.2d2c70db.png"
+  },
+  {
+    "revision": "29260ab8f8c5f4b4f3785934ee51ce35",
+    "url": "/mercury/static/media/storyboards.29260ab8.png"
+  },
+  {
+    "revision": "aee96ffd3d02c19c0f196b8f03d12ef0",
+    "url": "/mercury/static/media/superuser-consumer.aee96ffd.png"
+  },
+  {
+    "revision": "27e905e98e3929f67048ea18e53b45c2",
+    "url": "/mercury/static/media/superuser-merchant.27e905e9.png"
+  },
+  {
+    "revision": "e618dc81db686c736afad0a3d58d4a0d",
+    "url": "/mercury/static/media/surveys.e618dc81.png"
+  },
+  {
+    "revision": "4c053964fb2ee730f274b722727e5aa4",
+    "url": "/mercury/static/media/team-header.4c053964.png"
+  },
+  {
+    "revision": "7a3bbe0ccefd82d44a2462d5a6eaf408",
+    "url": "/mercury/static/media/team.7a3bbe0c.png"
+  }
+]);
